ae0783a99705d11007f469f445bef9c19a519461
Mert YILMAZ <yilmazmertm@gmail.com>
2020-07-04 00:05:48 +0300

    upload

---
+++ Assets/Ball.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+
+    Rigidbody2D rb2;
+    public float moveSpeed = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2 = GetComponent<Rigidbody2D>();
+        rb2.velocity = new Vector2(1, 0) * moveSpeed;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Racket.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Racket : MonoBehaviour
+{
+
+    string axisName = "Vertical1";
+    public float moveSpeed = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Debug.Log("Okey");
+        float moveAxis = Input.GetAxis(axisName)*moveSpeed;
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, moveAxis);
+    }
+}
